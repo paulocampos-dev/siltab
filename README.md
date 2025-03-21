7f0dd3271473607ce1af7ce2d460b91abff1d4ea
Paulo Campos <paulo.acampos@proton.me>
2025-03-21 15:56:50 -0300

    Refactoring Update

--- README.md
+++
@@ -1,5 +0,0 @@
-# Silver Tab
-
-Here is the code for our app in Kotlin.
-
-In the 'silver-tab-backend' directory is our Python API that manages PDI.
--- app/src/main/java/com/prototype/silver_tab/data/api_connection/routes/DealerRoutes.kt
+++
@@ -1,9 +0,0 @@
-package com.prototype.silver_tab.data.api_connection.routes
-
-import com.prototype.silver_tab.ui.components.DealerSummary
-import retrofit2.http.GET
-
-interface DealerApi {
-    @GET("dealer/user/dealer-summary")
-    suspend fun getDealerSummary(): List<DealerSummary>
-}
